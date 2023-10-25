ac268f03c2ad223c2049d80d6c456f822a4977b4
JavedZ <54533579+JavedZ@users.noreply.github.com>
2023-10-25 05:19:41 +0200

    testCommit
    just wanna se if it works

--- Assets/Scripts/TileDetector.cs
+++ Assets/Scripts/TileDetector.cs
@@ -1,19 +1,19 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class TileDetector : MonoBehaviour
-{
-    private Vector3 lastTilePosition;
+//public class TileDetector : MonoBehaviour
+//{
+//    private Vector3 lastTilePosition;
     
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Wall"))
-        {
-            MovePlayer.Instance.StopMoving(lastTilePosition);
-        }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.gameObject.CompareTag("Wall"))
+//        {
+//            MovePlayer.Instance.StopMoving(lastTilePosition);
+//        }
 
-        if (other.gameObject.CompareTag("Tile"))
-        {
-            lastTilePosition = new Vector3(other.gameObject.transform.position.x, 1, other.gameObject.transform.position.z);
-        }
-    }
-}
+//        if (other.gameObject.CompareTag("Tile"))
+//        {
+//            lastTilePosition = new Vector3(other.gameObject.transform.position.x, 1, other.gameObject.transform.position.z);
+//        }
+//    }
+//}
